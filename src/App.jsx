5db5cec2c5190475ccc965093039633de918798e
Siddharth Koundal <sksiddharth27@gmail.com>
2026-02-17 12:01:22 +0530

    add: search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import "./App.css";
 function App() {
   const [input, setInput] = useState("");
   const [recipes, setRecipes] = useState([]);
+  const [showRecipes, setShowRecipes] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch("https://dummyjson.com/recipes/search?q=");
+      const res = await fetch(
+        "https://dummyjson.com/recipes/search?q=" + input,
+      );
       const jsonData = await res.json();
       setRecipes(jsonData?.recipes);
     };
@@ -28,14 +31,18 @@ function App() {
           className="search-input"
           value={input}
           onChange={(e) => setInput(e.target.value)}
+          onFocus={() => setShowRecipes(true)}
+          onBlur={() => setShowRecipes(false)}
         />
-        <div className="search-results">
-          {recipes.map((recipe) => (
-            <div key={recipe.id} className="search-result">
-              {recipe.name}
-            </div>
-          ))}
-        </div>
+        {showRecipes && input && (
+          <div className="search-results">
+            {recipes.map((recipe) => (
+              <div key={recipe.id} className="search-result">
+                {recipe.name}
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
